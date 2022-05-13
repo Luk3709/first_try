73a2613c1108c68f0a40f6ece85e4181ac96468d
LF <lukas.faiss@web.de>
2022-05-13 10:44:11 +0200

    first working version of zipper

---
+++ code_1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def zipper(listA, listB):
+    zipped = zip(listA, listB)
+    
+    return zipped
+
+if __name__ == '__main__':
+    food = ["pizza","sushi","jam"]
+    people = ["Bob", "Kevin", "Angela"]
+    
+    result = zipper(food, people)
+    for item in result:
+        print(item)
